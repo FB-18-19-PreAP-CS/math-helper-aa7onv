e1bb5e5c333de89b50fc5d89ed823809d3dd9371
VERA.AARON <2200839@fbisd.flourbluffschools.net>
2018-09-12 14:07:39 -0500

    initial commit

---
+++ math_helper_vera_a.py
@@ -0,0 +1,12 @@
+from math import *
+
+def pythag_ther(a,b):
+    '''returns the hypotnuse of a right triangle'''
+    a = a * a
+    b = b * b
+    sums = a + b
+    ans = sqrt(sums)
+    
+    return "{:.2f}".format(ans)
+    
+pythag_ther(3,4)
