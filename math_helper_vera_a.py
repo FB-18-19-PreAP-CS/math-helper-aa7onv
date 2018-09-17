94a1eea7ebe6230a9aca365f584e677680105f28
VERA.AARON <2200839@fbisd.flourbluffschools.net>
2018-09-17 14:02:23 -0500

    3 formulas

--- math_helper_vera_a.py
+++ math_helper_vera_a.py
@@ -10,8 +10,25 @@ def pythag_ther(a,b):
     return "{:.2f}".format(ans)
 
 def slope(x1,y1,x2,y2):
+    '''returns the slope of 2 points'''
+    ans = (y1-y2)/(x1-x2)
+    
+    return "{:.2f}".format(ans)
+
+def arithmetic(a1,n,d):
+    '''returns last number in an arithmetic sequence'''
+    ans = a1+(d*(n-1))
+    
+    return "{:.2f}".format(ans)
+
+def sum_geometric(a,r,n):
+    '''returns the sum of a geometric sequence'''
     pass
+    
+    
 
 def main():    
     print(pythag_ther(3,4))
+    print(arithmetic(2,10,2))
+    
 main()
