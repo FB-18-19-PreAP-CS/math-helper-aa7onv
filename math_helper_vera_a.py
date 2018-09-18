c85e5214968a749fa41316b10ff739c671124e46
VERA.AARON <2200839@fbisd.flourbluffschools.net>
2018-09-18 14:12:36 -0500

    doctesting

--- math_helper_vera_a.py
+++ math_helper_vera_a.py
@@ -3,9 +3,15 @@ from math import *
 def pythag_ther(a,b):
     '''returns the hypotnuse of a right triangle
     >>> pythag_ther(3,4)
-    5.00
-    
-    
+    5.0
+    >>> pythag_ther(-2,5)
+    5.39
+    >>> pythag_ther(0,4)
+    4.0
+    >>> pythag_ther(1.1, 2.6)
+    2.82
+    >>> pythag_ther(-2,-2)
+    2.83
     
     
     '''
@@ -13,24 +19,46 @@ def pythag_ther(a,b):
     b = b * b
     sums = a + b
     ans = sqrt(sums)
+    ans = round(ans,2)
     
-    return "{:.2f}".format(ans)
+    return ans
 
 def slope(x1,y1,x2,y2):
-    '''returns the slope of 2 points'''
-    ans = (y1-y2)/(x1-x2)
+    '''returns the slope of 2 points
+    >>> slope(0,1,0,3)
+    Traceback (most recent call last):
+        ...
+    ValueError: denominator cannot equal 0
+    >>> slope(1,2,3,4)
+    1.0
     
-    return "{:.2f}".format(ans)
+    
+    
+    '''
+    yans = (y1-y2)
+    xans =(x1-x2)
+    #if xans == 0:
+      #  return ValueError:
+    ans = round(ans,2)
+    
+    return ans
 
 def arithmetic(a1,n,d):
     '''returns last number in an arithmetic sequence'''
     ans = a1+(d*(n-1))
+    ans = round(ans,2)
     
-    return "{:.2f}".format(ans)
+    return ans
 
 def sum_geometric(a,r,n):
     '''returns the sum of a geometric sequence'''   
-    pass    
+    pass
+
+def eccentricity_ellipse(a,b):
+    pass
+
+def velocity():
+    pass
     
 if __name__ =="__main__":
     import doctest
